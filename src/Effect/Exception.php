80ac52ffe3ff3bb6e947cd02e6b2b2b3ca2e97f8
0x1 <kevin.francart3@gmail.com>
2026-07-23 11:18:27 +0200

    Clean up unused by-reference parameter captures in closures

--- src/Effect/Exception.php
+++ src/Effect/Exception.php
@@ -1,22 +1,22 @@
 <?php
 
-$error = function($msg) use (&$error) { return new \Exception($msg); };
-$errorWithCause = function($msg) use (&$errorWithCause) {
+$error = function($msg) { return new \Exception($msg); };
+$errorWithCause = function($msg) {
     return function($cause) use ($msg) {
         return new \Exception($msg, 0, $cause instanceof \Throwable ? $cause : null);
     };
 };
-$errorWithName = function($msg) use (&$errorWithName) {
+$errorWithName = function($msg) {
     return function($name) use ($msg) {
         $e = new class($msg) extends \Exception { public $name; };
         $e->name = $name;
         return $e;
     };
 };
-$message = function($e) use (&$message) { return $e->getMessage() . "\n" . $e->getTraceAsString(); };
-$name = function($e) use (&$name) { return isset($e->name) ? $e->name : \get_class($e); };
-$stackImpl = function($just) use (&$stackImpl) { return function($nothing) { return function($e) use(&$just, &$nothing) { return $just($e->getTraceAsString()); }; }; };
-$throwException = function($e) use (&$throwException) { return function() use(&$e) { throw $e; }; };
+$message = function($e) { return $e->getMessage() . "\n" . $e->getTraceAsString(); };
+$name = function($e) { return isset($e->name) ? $e->name : \get_class($e); };
+$stackImpl = function($just) { return function($nothing) use ($just) { return function($e) use($just, $nothing) { return $just($e->getTraceAsString()); }; }; };
+$throwException = function($e) { return function() use($e) { throw $e; }; };
 $catchException = function($c, $t = null) use (&$catchException) {
     if (\func_num_args() < 2) {
         $__args = \func_get_args();
@@ -25,9 +25,9 @@ $catchException = function($c, $t = null) use (&$catchException) {
             return $catchException(...\array_merge($__args, $more));
         };
     }
-    return function() use(&$c, &$t) { try { return $t(); } catch (\Throwable $e) { if (strpos($e->getMessage(), 'Object of class stdClass') !== false) { echo "\n\n!!! CAUGHT IN EXCEPTION.PHP:\n" . $e->getTraceAsString() . "\n\n"; } return $c($e)(); } };
+    return function() use($c, $t) { try { return $t(); } catch (\Throwable $e) { return $c($e)(); } };
 };
-$showErrorImpl = function($e) use (&$showErrorImpl) { return (string)$e; };
+$showErrorImpl = function($e) { return (string)$e; };
 
 $exports['error'] = $error;
 $exports['errorWithCause'] = $errorWithCause;
